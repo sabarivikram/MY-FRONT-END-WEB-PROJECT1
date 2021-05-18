5d1e8416ee5950d7ebdaf3ab9dc004a3e4a9e6c5
sabari vikram <vikramsabari2000r@gmail.com>
2021-05-18 13:22:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,9 +15,62 @@ $(document).ready(function(){
   });
 
   $('ul.parent li').hover(function(){
-    $(this).find('ul.child').show(200);
+    $(this).find('ul.child').show();
      },function () {
-    $(this).find('ul.child').hide(200);
+    $(this).find('ul.child').hide();
   });
+  $('#place-order').on('click',function(e){
+    e.preventDefault();
+    console.log('prevented');
+  });
+
+  $(document).on('mousedown',function(event){
+    event.stopPropagation();
+    switch (event.which) {
+      case 1:
+        console.log('left clicked', event.pageY, event.pageX);
+        break;
+      case 2:
+        console.log('middle clicked', event.pageY, event.pageX);
+        break;
+      case 3:
+        console.log('right clicked', event.pageY, event.pageX);
+        break;
+      default:
+          console.log('error');
+    }
+  });
+
+
+/*  $(document).on('contextmenu',function(){
+    return false;
+  });
+
+
+ $(document).on('mousedown',function(con){
+  con.preventDefault();
+
+    if(con.which== 3){
+
+      $('.hidden').fadeOut();
+
+      if($(con.target).is('img')){
+        console.log('img clicked');
+        $('.opentab').fadeIn();
+      }
+
+      $('#context').css({
+        top:con.pageY,left:con.pageX
+      });
+
+      $('#context').fadeIn();
+      return false;
+    }
+  $('#context').fadeOut();
+});
+*/
+
+
+
 
 });
